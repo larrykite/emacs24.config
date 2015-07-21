c6c60a941e78624a4203b10803df0c2c6ebac5aa
Larry Kite <larry@skytree.net>
2015-07-21 16:58:12 -0700

    No idea. Bunch of changes

--- init.el
+++ init.el
@@ -1,9 +1,7 @@
-
-;; Time-stamp: <Last changed 30-05-2014 08:45:52 by Larry Kite, larrykite>
+;; Time-stamp: <Last changed 03-07-2015 17:05:19 by Larry Kite, larrykite>
 
 (defconst *is-a-mac* (eq system-type 'darwin))
 (defconst *is-linux* (eq system-type 'gnu/linux))
-
 
 ;; Configure el-get
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
@@ -21,7 +19,7 @@
       (goto-char (point-max))
       (eval-print-last-sexp))))
 
-; list all packages you want installed
+;; list all packages you want installed
 (setq my-el-get-packages
       (append
        '(auto-complete
@@ -44,7 +42,6 @@
 (defvar my-packages '(starter-kit
                       starter-kit-bindings
                       ace-jump-mode
-                      ack-and-a-half
                       browse-kill-ring
                       buffer-move
                       deft
@@ -184,12 +181,8 @@
 
 (defalias 'qrr 'query-replace-regexp)
 
-(set-frame-height (selected-frame) 52)
-(set-frame-width (selected-frame) 130)
-
-
 (fset 'resize-main-frame
-   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([67108917 67108915 3 104 67108913 67108915 67108912 3 119] 0 "%d")) arg)))
+      (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([67108917 67108915 3 104 67108913 67108915 67108912 3 119] 0 "%d")) arg)))
 (global-set-key (kbd "C-c z") 'resize-main-frame)
 
 
@@ -204,51 +197,18 @@
 (setq-default header-line-format
               '((which-func-mode ("" which-func-format " "))))
 (setq mode-line-misc-info
-            ;; We remove Which Function Mode from the mode line, because it's mostly
-            ;; invisible here anyway.
-            (assq-delete-all 'which-func-mode mode-line-misc-info))
+      ;; We remove Which Function Mode from the mode line, because it's mostly
+      ;; invisible here anyway.
+      (assq-delete-all 'which-func-mode mode-line-misc-info))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
- '(custom-safe-themes
+ '(package-selected-packages
    (quote
-    ("e53cc4144192bb4e4ed10a3fa3e7442cae4c3d231df8822f6c02f1220a0d259a" "e80a0a5e1b304eb92c58d0398464cd30ccbc3622425b6ff01eea80e44ea5130e" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "ae8d0f1f36460f3705b583970188e4fbb145805b7accce0adb41031d99bd2580" "1affe85e8ae2667fb571fc8331e1e12840746dae5c46112d5abb0c3a973f5f5a" "7694bfb46ec19cfc47093783633d8cd4df208d620104910bf5c1c840528a8dd1" "de2c46ed1752b0d0423cde9b6401062b67a6a1300c068d5d7f67725adc6c3afb" "978ff9496928cc94639cb1084004bf64235c5c7fb0cfbcc38a3871eb95fa88f6" "41b6698b5f9ab241ad6c30aea8c9f53d539e23ad4e3963abff4b57c0f8bf6730" "9bac44c2b4dfbb723906b8c491ec06801feb57aa60448d047dbfdbd1a8650897" "405fda54905200f202dd2e6ccbf94c1b7cc1312671894bc8eca7e6ec9e8a41a2" "51bea7765ddaee2aac2983fac8099ec7d62dff47b708aa3595ad29899e9e9e44" "3d6b08cd1b1def3cc0bc6a3909f67475e5612dba9fa98f8b842433d827af5d30" "8c5ffc9848db0f9ad4e296fa3cba7f6ea3b0e4e00e8981a59592c99d21f99471" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(deft-directory "/home/larry/Dropbox/deft/")
- '(ein:use-auto-complete t)
- '(ein:use-auto-complete-superpack t)
- '(fci-rule-color "#383838")
- '(ido-create-new-buffer (quote never))
- '(ido-enable-flex-matching t)
- '(ido-everywhere t)
- '(ido-file-extensions-order (quote (".py" ".el" ".sh")))
- '(ido-show-dot-for-dired t)
- '(ido-use-filename-at-point (quote guess))
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#BC8383")
-     (40 . "#CC9393")
-     (60 . "#DFAF8F")
-     (80 . "#D0BF8F")
-     (100 . "#E0CF9F")
-     (120 . "#F0DFAF")
-     (140 . "#5F7F5F")
-     (160 . "#7F9F7F")
-     (180 . "#8FB28F")
-     (200 . "#9FC59F")
-     (220 . "#AFD8AF")
-     (240 . "#BFEBBF")
-     (260 . "#93E0E3")
-     (280 . "#6CA0A3")
-     (300 . "#7CB8BB")
-     (320 . "#8CD0D3")
-     (340 . "#94BFF3")
-     (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
+    (ess web-mode zenburn-theme switch-window solarized-theme smartscan rainbow-delimiters naquadah-theme multiple-cursors key-chord jinja2-mode ido-vertical-mode git-gutter-fringe git-gutter helm expand-region elpy dropdown-list discover dired+ deft buffer-move browse-kill-ring ace-jump-mode starter-kit-bindings))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -259,8 +219,8 @@
 (put 'upcase-region 'disabled nil)
 
 (fset 'resize-main-frame
-   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([67108917 67108915 3 104 67108913 67108915 67108912 3 119] 0 "%d")) arg)))
+      (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([67108917 67108915 3 104 67108913 67108915 67108912 3 119] 0 "%d")) arg)))
 (global-set-key (kbd "C-c z") 'resize-main-frame)
 
 (fset 'testmacro
-   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("" 0 "%d")) arg)))
+      (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("" 0 "%d")) arg)))
